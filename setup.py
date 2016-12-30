6e2325ad7c30c74fd5f8b3d82aef480d5acfcfa6
Jeremy Tan <jtanx@outlook.com>
2016-12-30 13:02:51 +0800

    ????

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setup(
         ':python_version=="2.6" or python_version=="2.7"': ['ipaddr>=2.1', 'configparser', 'werkzeug>=0.8'],
         ':python_version!="2.6" and python_version!="2.7"': ['werkzeug>=0.9']
     },
-    setup_requires=["cffi>=1.3.0"],
     entry_points={
         'console_scripts':
             ['isso = isso:main'],
